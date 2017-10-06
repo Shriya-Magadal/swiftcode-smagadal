8b4831baa83aeae64bf24dc64a457142cef73a99
pranathi <pranathibonagiri96@gmail.com>
2017-10-06 10:14:04 +0000

    adding classes

--- app/data/FeedResponse.java
+++ app/data/FeedResponse.java
@@ -1,5 +1,7 @@
 package data;
 
 public class FeedResponse {
+    String text;
+
 
 }
--- app/data/Message.java
+++ app/data/Message.java
@@ -1,5 +1,10 @@
 package data;
 
 public class Message {
+    public enum Sender{BOT,USER}
+    public String text;
+    public Sender sender;
+    public String time;
+
 
 }
