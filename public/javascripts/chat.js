05ceb40cf3c0d1cbd5ade4becfcacd46cb647992
Shriya <magdalshriya@gmail.com>
2017-10-08 05:30:52 +0000

    styling

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,4 +1,17 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope, $sce) {
+    $scope.messages=[
+    {'sender':'USER',
+    'text':'Hi',
+    'time':'10:01 AM'
+    },
+    {'sender':'BOT',
+        'text':'Hi',
+        'time':'10:02 AM'
+    },
+    {'sender':'USER',
+            'text':'Hello',
+            'time':'10:03 AM'
+    }]
 });
