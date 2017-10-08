ce9ea5e594be773ed88947e86a5b28c214a047e7
Shriya <magdalshriya@gmail.com>
2017-10-08 07:37:18 +0000

    now it shows news

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,17 +1,21 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope, $sce) {
-    $scope.messages=[
-    {'sender':'USER',
-    'text':'Hi',
-    'time':'10:01 AM'
-    },
-    {'sender':'BOT',
-        'text':'Hi',
-        'time':'10:02 AM'
-    },
-    {'sender':'USER',
-            'text':'Hello',
-            'time':'10:03 AM'
-    }]
+    $scope.messages=[];
+
+var  exampleSocket =  new  WebSocket("wss://swiftcode-ws-chat.herokuapp.com/chatSocket");
+    exampleSocket.onmessage  =   function  (event) {
+        var jsonData = JSON.parse(event.data);
+        jsonData.time = new Date()
+            .toLocaleTimeString();
+        $scope.messages.push(jsonData);
+        $scope.$apply();
+        console.log(jsonData);
+    };
+    $scope.sendMessage = function () {
+            exampleSocket.send($scope.userMessage);
+            $scope.userMessage = "";
+        };
+
+        $scope.trust=$sce.trustAsHtml;
 });
